e22e38dd547cb8d14b71328c3930b8045c0460fc
abilmansur <abilmuzhubayev@gmail.com>
2022-09-17 16:46:35 +0600

    Initial commit

---
+++ src/main/java/com/asphyxia/routList/RoutListApplication.java
@@ -0,0 +1,13 @@
+package com.asphyxia.routList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asphyxia/routList/RoutListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asphyxia.routList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
