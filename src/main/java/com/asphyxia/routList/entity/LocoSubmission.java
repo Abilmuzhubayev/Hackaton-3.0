bcb57431412d11f3d5c9922df1b4cff9225cbe69
adilkhanm <adilkhan.muratov@gmail.com>
2022-09-18 16:41:54 +0600

    refactor

--- src/main/java/com/asphyxia/routList/entity/LocoSubmission.java
+++ src/main/java/com/asphyxia/routList/entity/LocoSubmission.java
@@ -13,6 +13,7 @@ import java.util.List;
 @Data
 public class LocoSubmission {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "loco_submission_id")
     private Long id;
 
