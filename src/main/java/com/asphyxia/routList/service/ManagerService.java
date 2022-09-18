ee98174290676858460d1bae6d6a0fd4c3f9c32e
adilkhanm <adilkhan.muratov@gmail.com>
2022-09-18 10:33:26 +0600

    added manager service methods

--- src/main/java/com/asphyxia/routList/service/ManagerService.java
+++ src/main/java/com/asphyxia/routList/service/ManagerService.java
@@ -1,10 +1,63 @@
 package com.asphyxia.routList.service;
 
+import com.asphyxia.routList.dao.ManagerDao;
+import com.asphyxia.routList.dto.*;
+import com.asphyxia.routList.entity.Route;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ManagerService {
 
+    @Autowired
+    private ManagerDao managerDao;
 
+    @Autowired
+    private RouteConverter routeConverter;
 
+    @Transactional
+    public List<RouteCardDto> getRoutes(Long managerId) {
+        List<Route> routes = managerDao.getRoutes();
+        return routes.stream().map(x -> routeConverter.getDto(x)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<TaskDto> getRouteTasks(Long routeId) {
+    }
+
+    @Transactional
+    public SubtaskDto getRouteSubtask(Long subtaskId) {
+    }
+
+    @Transactional
+    public StationDataDto getStationData(Long stationDataId) {
+    }
+
+    @Transactional
+    public LocoSubmissionDto getLocoSubmission(Long locoSubmissionId) {
+    }
+
+    @Transactional
+    public LocoAcceptanceDto getLocoAcceptance(Long locoAcceptanceId) {
+    }
+
+    @Transactional
+    public RouteDetailsDto getRouteDetails(Long routeId) {
+    }
+
+    @Transactional
+    public void saveRoute(CreateRouteDto createRouteDto) {
+    }
+
+    @Transactional
+    public List<DriverDto> getDrivers() {
+    }
+
+    @Transactional
+    public List<StationDto> getStations() {
+    }
 }
